1f9bc9d86f29a4b05add65901e074843ec21bd1e
Muhammadumar <muxtorjonovzzz@gmail.com>
2026-07-14 12:46:40 +0500

    first commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('portfolio_details/', views.portfolio_details, name='portfolio_details'),
 ]
