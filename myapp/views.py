51c1457cf7e088e5fe181c80aeaeba10139e3141
Muhammadumar <muxtorjonovzzz@gmail.com>
2026-07-28 23:16:35 +0500

    first commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from myapp.models import Portfolio, Type, Service, TeamMember, Murojaat, Subscribe
+from django.db.models import Q
 # Create your views here.
 
+from django.shortcuts import render
+from django.db.models import Q
+from .models import Portfolio, Type, Service, TeamMember, Murojaat
+
 def home(request):
-    if request.method == "POST":
+    if request.method == "POST" and 'subject' in request.POST:
         ismi = request.POST.get('name')
         mail = request.POST.get('email')
         mavzu = request.POST.get('subject')
@@ -14,20 +19,45 @@ def home(request):
             mail=mail,
             title=mavzu,
             text=xabar
-        ).save()
+        )
 
-    works = Portfolio.objects.all()
-    turlar = Type.objects.all()
-    portfolios = Portfolio.objects.all()
     services = Service.objects.filter(is_active=True)
+    works = Portfolio.objects.all()
     team_members = TeamMember.objects.filter(is_active=True)
+
+    if request.method == "POST" and 'soz' in request.POST:
+        text = str(request.POST.get('soz', '')).strip()
+        
+        if text:
+            services = Service.objects.filter(
+                Q(title__icontains=text) | Q(description__icontains=text),
+                is_active=True
+            )
+            
+            works = Portfolio.objects.filter(
+                Q(nomi__icontains=text) | 
+                Q(company_name__icontains=text) |
+                Q(date__icontains=text) | 
+                Q(url__icontains=text) |
+                Q(malumot__icontains=text) | 
+                Q(tur__nomi__icontains=text)
+            )
+
+            team_members = TeamMember.objects.filter(
+                Q(full_name__icontains=text) | 
+                Q(position__icontains=text) | 
+                Q(bio__icontains=text),
+                is_active=True
+            )
+
     context = {
-            "portfolios": portfolios,
-            "services": services,
-            "team_members": team_members,
-            'works': works, 
-            'types': turlar,
-        }
+        "portfolios": works,
+        "services": services,
+        "team_members": team_members,
+        "works": works,
+        "types": Type.objects.all(),
+    }
+    
     return render(request, 'myapp/index.html', context)
 
 def filter_index(malumot, id):
